032b73ba75ed424cf8e0dfb47222b94da2449ad5
SagnikAcharya <sagnikacharya17@gmail.com>
2022-11-03 22:51:43 +0530

    Final commit

--- js/script.js
+++ js/script.js
@@ -6,3 +6,12 @@ btn.addEventListener('click', () => {
     nav.classList.toggle('flex');
     nav.classList.toggle('hidden');
 })
+
+function validateemail() {
+    var x = document.form1.text1.value;
+    var atposition = x.indexOf("@");
+    var dotposition = x.lastIndexOf(".");
+    if (atposition < 1 || dotposition < atposition + 2 || dotposition + 2 >= x.length) {
+        alert("Please enter a valid e-mail address");
+    }
+}
